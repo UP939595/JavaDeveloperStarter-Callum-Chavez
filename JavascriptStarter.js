58a3c11736c8dbceb7406b3526f16a853665b797
Callum Chavez <72569556+UP939595@users.noreply.github.com>
2021-08-03 11:04:46 +0100

    Add files via upload

---
+++ JavascriptStarter.js
@@ -0,0 +1,18 @@
+test1 = 3;
+
+let Person = new Object();
+Person.name = 'Adam';
+Person.age = 20;
+
+console.log(Person.name, Person.age);
+
+let fruits = ['Apple', 'Banana', 'Pear', 'Melon'];
+console.log(fruits);
+
+// 1 - if
+// 2 - if
+// 3 - else
+
+function print(a, b) {
+    console.log(a, b);   // The function returns the product of p1 and p2
+  }
